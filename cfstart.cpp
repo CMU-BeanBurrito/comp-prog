475582c7c983e98a016ae47626685c3ba470391e
unknown <raynorcat@gmail.com>
2024-07-31 19:56:10 -0400

    added starter file

---
+++ cfstart.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long; 
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+
+void solve() {
+    
+}
+
+int main() {
+    int t; scanf("%d\n", t);
+    
+    while(t--) {
+        solve();
+    }
+    
+    return 0;
+}
+
+
+
