8cd6c045294bcc69d54d200c7678dd672d948f28
unknown <raynorcat@gmail.com>
2024-09-01 19:03:32 -0400

    added modular inverse to start file

--- acstart.cpp
+++ acstart.cpp
@@ -13,6 +13,33 @@ using ll = long long;
 
 #define MOD 1'000'000'007
 
+int gcdExt(int a, int b, int* x, int* y)
+{
+    if (a == 0)
+    {
+        *x = 0;
+        *y = 1;
+        return b;
+    }
+    else
+    {
+        int x1, y1;
+        int gcd = gcdExt(b % a, a, &x1, &y1);
+
+        *x = y1 - (b/a) * x1;
+        *y = x1;
+
+        return gcd;
+    }
+}
+int inv (int a, int m) // modular inverse of a mod m
+{
+    int x, y;
+    int g = gcdExt(a, m, &x, &y);
+
+    return (x % m + m) % m;
+}
+
 void solve() {
     
 }
