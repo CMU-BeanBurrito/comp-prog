542222c294276b91292d4f45a9f8534864b1fb6a
unknown <raynorcat@gmail.com>
2024-09-14 01:50:04 -0400

    2001

---
+++ CF/CF2001/B.cpp
@@ -0,0 +1,77 @@
+/*
+    Author: BeanBurrito
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+using ll = long long;
+
+#define MOD 1'000'000'007
+
+int gcdExt(int a, int b, int* x, int* y);
+int inv (int a, int m);
+
+void solve() {
+    int n; cin >> n;
+
+    if (n % 2 == 0)
+    {
+        printf("-1\n");
+        return;
+    }
+
+    for (int i = 0; i < n/2; i++)
+    {
+        printf("%d ", n-1-2*i);
+    }
+
+    for (int i = n/2; i < n; i++)
+    {
+        printf("%d ", 1 + 2*(i-n/2));
+    } printf("\n");
+}
+
+int main() {
+    int t; cin >> t;
+    
+    while(t--) {
+        solve();
+    }
+    
+    return 0;
+}
+
+int gcdExt(int a, int b, int* x, int* y)
+{
+    if (a == 0)
+    {
+        *x = 0;
+        *y = 1;
+        return b;
+    }
+    else
+    {
+        int x1, y1;
+        int gcd = gcdExt(b % a, a, &x1, &y1);
+
+        *x = y1 - (b/a) * x1;
+        *y = x1;
+
+        return gcd;
+    }
+}
+int inv (int a, int m) // modular inverse of a mod m
+{
+    int x, y;
+    int g = gcdExt(a, m, &x, &y);
+
+    return (x % m + m) % m;
+}
+
+
