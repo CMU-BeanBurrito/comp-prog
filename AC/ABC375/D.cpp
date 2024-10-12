981bdfc08f6ea86d5a6e1024f5fd83cdd92a3689
unknown <raynorcat@gmail.com>
2024-10-12 11:01:40 -0400

    abc375

---
+++ AC/ABC375/D.cpp
@@ -0,0 +1,117 @@
+/*
+    Author: BeanBurrito
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+using ll = long long;
+
+#define MOD 1'000'000'007
+#define divceil(n, m) (n+m-1)/m
+
+int gcdExt(int a, int b, int* x, int* y);
+int inv (int a, int m);
+ll kadane(vector<int>& a, int n);
+ll gcd(ll a, ll b);
+ll lcm(ll a, ll b);
+
+void solve() {
+    string s; cin >> s;
+
+    vector<vector<int>> a (26, vector<int>());
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        a[s[i]-'A'].push_back(i);
+    }
+
+    ll ans = 0LL;
+
+    for (int i = 0; i < 26; i++)
+    {
+        int m = a[i].size();
+        if (m < 2) continue;
+
+        ll acc = 0LL;
+
+        for (int j = 1; j < m; j++)
+        {
+            ans += 1LL*(j-1)*(j)/2;
+            ans += acc;
+            ans += 1LL* (a[i][j]-a[i][j-1]-1) * (j);
+            acc += 1LL* (a[i][j]-a[i][j-1]-1) * (j);
+        }
+    }
+
+    printf("%lld\n", ans);
+}
+
+int main() {
+    solve();
+    return 0;
+}
+
+int gcdExt(int a, int b, int* x, int* y)
+{
+    if (a == 0)
+    {
+        *x = 0;
+        *y = 1;
+        return b;
+    }
+    else
+    {
+        int x1, y1;
+        int gcd = gcdExt(b % a, a, &x1, &y1);
+
+        *x = y1 - (b/a) * x1;
+        *y = x1;
+
+        return gcd;
+    }
+}
+int inv (int a, int m) // modular inverse of a mod m
+{
+    int x, y;
+    int g = gcdExt(a, m, &x, &y);
+
+    return (x % m + m) % m;
+}
+ll kadane(vector<int>& a, int n)
+{
+    ll sum = 0;
+    ll best = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (sum > 0)
+        {
+            sum += a[i];
+        }
+        else
+        {
+            sum = a[i];
+        }
+
+        best = max(best, sum);
+    }
+
+    return best;
+}
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0) return a;
+    return gcd (b, a%b);
+}
+ 
+ll lcm(ll a, ll b)
+{
+    return (a / gcd(a, b)) * b;
+}
