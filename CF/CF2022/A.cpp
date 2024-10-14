0111e97a5461d5586b963bd688a126c53c370195
unknown <raynorcat@gmail.com>
2024-10-13 20:30:30 -0400

    .

---
+++ CF/CF2022/A.cpp
@@ -0,0 +1,118 @@
+/*
+    Author: BeanBurrito
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+using ll = long long;
+
+#define MOD 1'000'000'007
+#define divceil(n, m) (n+m-1)/m
+
+int gcdExt(int a, int b, int* x, int* y);
+int inv (int a, int m);
+ll kadane(vector<int>& a, int n);
+ll gcd(ll a, ll b);
+ll lcm(ll a, ll b);
+
+void solve() {
+    int n, r; cin >> n >> r;
+    vector<int> a (n);
+
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    int ans = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        ans += 2*min(r, a[i]/2);
+        r  -= min(r, a[i]/2);
+        a[i] %= 2;
+    }
+
+    int single = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == 1) single++;
+    }
+
+    ans += min(single, 2*r - single);
+
+    printf("%d\n", ans);
+}
+
+int main() {
+    int t; cin >> t;
+    
+    while(t--) {
+        solve();
+    }
+    
+    return 0;
+}
+
+int gcdExt(int a, int b, int* x, int* y)
+{
+    if (a == 0)
+    {
+        *x = 0;
+        *y = 1;
+        return b;
+    }
+    else
+    {
+        int x1, y1;
+        int gcd = gcdExt(b % a, a, &x1, &y1);
+
+        *x = y1 - (b/a) * x1;
+        *y = x1;
+
+        return gcd;
+    }
+}
+int inv (int a, int m) // modular inverse of a mod m
+{
+    int x, y;
+    int g = gcdExt(a, m, &x, &y);
+
+    return (x % m + m) % m;
+}
+ll kadane(vector<int>& a, int n)
+{
+    ll sum = 0;
+    ll best = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (sum > 0)
+        {
+            sum += a[i];
+        }
+        else
+        {
+            sum = a[i];
+        }
+
+        best = max(best, sum);
+    }
+
+    return best;
+}
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0) return a;
+    return gcd (b, a%b);
+}
+ 
+ll lcm(ll a, ll b)
+{
+    return (a / gcd(a, b)) * b;
+}
