196fb0947f550e9cb53cb616843d7c6c4225f258
unknown <raynorcat@gmail.com>
2024-10-18 00:30:00 -0400

    -

---
+++ ac2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir AC/$1
+cp cfstart.cpp AC/$1/A.cpp
+cp cfstart.cpp AC/$1/B.cpp
+cp cfstart.cpp AC/$1/C.cpp
+
---
+++ cf2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir CF/CF$1
+cp cfstart.cpp CF/CF$1/A.cpp
+cp cfstart.cpp CF/CF$1/B.cpp
+cp cfstart.cpp CF/CF$1/C.cpp
+
