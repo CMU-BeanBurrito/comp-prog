e610e28eb9eae225a510db99a9c8cbcb1daf8d51
unknown <raynorcat@gmail.com>
2024-11-15 16:05:30 -0500

    .

---
+++ CF/CF2033/D.cpp
@@ -0,0 +1,232 @@
+/*
+    Author: BeanBurrito
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+using ll = long long;
+
+#define MOD 1'000'000'007
+#define divceil(n, m) (n+m-1)/m
+
+int gcdExt(int a, int b, int* x, int* y);
+int inv (int a, int m); // modular inverse of a mod m
+ll kadane(vector<int>& a, int n); // maximum subarray sum
+ll gcd(ll a, ll b);
+ll lcm(ll a, ll b);
+ll merge(vector<int>& v, int l, int mid, int r); // l and r are inclusive (e.g. 0, n-1)
+ll mergesort(vector<int>& v, int l, int r); // l and r are inclusive (e.g. 0, n-1), returns inversion count
+void printi(vector<int>& v); // print vector of ints
+void printll(vector<ll>& v); // print vector of long long ints
+void printc(vector<char>& v); // print vector of chars
+void printiimp(map<int, int>& mp); // print int, int map
+void printcimp(map<int, int>& mp); // print char, int map
+bool sort2nd(const pair<ll, ll> &p1, const pair<ll, ll> &p2);
+
+// PUT GLOBALS HERE
+
+void solve() {
+    int n; cin >> n;
+    vector<int> a (n+1);
+
+    for (int i = 1; i <= n; i++) cin >> a[i];
+
+    vector<ll> pref (n+1);
+    pref[0] = 0LL;
+
+    for (int i = 1; i <= n; i++)
+    {
+        pref[i] = pref[i-1] + a[i];
+    }
+
+    map<ll, int> mp;
+    mp[0LL]++;
+    int seg = 0;
+    ll last = 0LL;
+
+    for (int i = 1; i <= n; i++)
+    {
+        pref[i] -= last;
+
+        if (a[i] == 0 || (mp[pref[i]] > 0))
+        {
+            seg++;
+            mp.clear();
+            mp[0]++;
+            last += pref[i];
+        }
+        else
+        {
+            mp[pref[i]]++;
+        }
+
+        //printf("ITH %d %d %lld\n", i, seg, last);
+        //for (auto p : mp) cout << p.first << " " << p.second << endl;
+    }
+
+    printf("%d\n", seg);
+}
+
+int main() {
+    int t; cin >> t;
+    while(t--) solve();
+    return 0;
+}
+
+int gcdExt(int a, int b, int* x, int* y)
+{
+    if (a == 0)
+    {
+        *x = 0;
+        *y = 1;
+        return b;
+    }
+    else
+    {
+        int x1, y1;
+        int gcd = gcdExt(b % a, a, &x1, &y1);
+
+        *x = y1 - (b/a) * x1;
+        *y = x1;
+
+        return gcd;
+    }
+}
+
+int inv (int a, int m)
+{
+    int x, y;
+    gcdExt(a, m, &x, &y);
+
+    return (x % m + m) % m;
+}
+
+ll kadane(vector<int>& a, int n)
+{
+    ll sum = 0;
+    ll best = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (sum > 0)
+        {
+            sum += a[i];
+        }
+        else
+        {
+            sum = a[i];
+        }
+
+        best = max(best, sum);
+    }
+
+    return best;
+}
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0) return a;
+    return gcd (b, a%b);
+}
+ 
+ll lcm(ll a, ll b)
+{
+    return (a / gcd(a, b)) * b;
+}
+
+ll merge(vector<int>& v, int l, int mid, int r) {
+    int lidx = l;
+    int ridx = mid; // mid+1 from mergesort, first elem of right side
+    int residx = 0;
+    vector<int> res (r-l+1);
+    
+    ll ans = 0;
+    
+    while(lidx <= mid-1 && ridx <= r) {
+        if (v[lidx] <= v[ridx]) {
+            res[residx] = v[lidx];
+            lidx++; residx++;
+        } else { // inversion
+            res[residx] = v[ridx];
+            ridx++; residx++;
+            
+            ans += (mid-lidx); // elements lidx to mid-1 are all greater than v[ridx]
+        }
+    }
+    
+    while(lidx <= mid-1) {
+        res[residx] = v[lidx];
+        lidx++; residx++;
+    }
+    
+    while(ridx <= r) {
+        res[residx] = v[ridx];
+        ridx++; residx++;
+    }
+    
+    for (int i = l; i <= r; i++) {
+        v[i] = res[i-l];
+    }
+    
+    return ans;
+}
+ 
+ll mergesort(vector<int>& v, int l, int r) {
+    ll ans = 0;
+    
+    if (l < r) {
+        int mid = l + (r-l)/2; //mid is part of the left subarray
+        
+        ans += mergesort(v, l, mid);
+        ans += mergesort(v, mid+1, r);
+        
+        ans += merge(v, l, mid+1, r);
+    }
+    
+    return ans;
+}
+
+void printi(vector<int>& v)
+{
+    for (int x : v)
+    {
+        printf("%d ", x);
+    } printf("\n");
+}
+
+void printll(vector<ll>& v)
+{
+    for (ll x : v)
+    {
+        printf("%lld ", x);
+    } printf("\n");
+}
+
+void printc(vector<char>& v)
+{
+    for (char x : v)
+    {
+        printf("%c ", x);
+    } printf("\n");
+}
+
+void printiimp(map<int, int>& mp)
+{
+    for (auto p : mp) printf("%d %d\n", p.first, p.second);
+}
+
+void printcimp(map<char, int>& mp)
+{
+    for (auto p : mp) printf("%c %d\n", p.first, p.second);
+}
+
+bool sort2nd(const pair<ll, ll> &p1, const pair<ll, ll> &p2)
+{
+    return p1.second < p2.second;
+}
---
+++ CF/CF2033/Notes.md
@@ -0,0 +1,48 @@
+Notes on solutions/approaches to problems
+
+# A: Sakurako and Kosuke
+While this can be done in constant time, using the parity of n, during the contest I chose to just simulate it.
+
+# B: Sakurako and Water
+If two or more lakes are on the same "down-right" diagonal, their heights can be changed simultaneously. Furthermore, this is the only condition under which two (or more) lakes can have their heights changed simultaneously.
+
+So, for each down-right diagonal, we find the deepest lake with height h. Then we can "fix" all lakes on this diagonal with |h| operations.
+
+Simply sum the total number of operations for all diagonals.
+
+# C: Sakurako's Field Trip
+Greedy solution - start on the outside (start and end) of the array and work inwards towards the middle. If swapping a pair is better than the current state, do so.
+
+Sum up the disturbance and return.
+
+# D: Kousuke's Assignment
+
+Greedy solution - if we find a beautiful segment, take it, since any segments that we haven't seen that use previous elements will overlap with this segment.
+
+If we do find a segment, afterwards, we only consider the suffix of the original array after this segment.
+
+In this suffix, keep track of all prefixes we have seen. If we see this prefix again, it means that there was a subarray (segment) that was beautiful. Reset our map since we no longer consider previous elements
+
+# E: Sakurako, Kosuke, and the Permutation
+Re-frame the permutation as a graph. There are n nodes numbered 1 to n.
+
+There is an edge from i to j if and only if p[i] = j. There will be n edges, each node will have 1 edge entering it and 1 edge exiting it.
+
+Based on the problem statement, we need every node to satisfy one of these conditions:
+- Be in its own cycle of 1 (edge pointing from itself, to itself)
+- Be in a cycle of 2 (2 nodes each pointing to each other).
+
+If we have a cycle of x nodes, we need to break it up into cycle of at most 2 nodes. In other words, we need to remove x-2 nodes. 
+
+How does swapping affect this graph? Swapping a and b redirects 2 edges since the edge pointing from u to a now points from u to b, and the edge pointing from v to b now points from v to a. This will also cause a split into 2 different cycles of size x-y and y. So, we need to do (x-2)/2, rounded up, operations to resolve a cycle of x > 2.
+
+The answer is the sum for all cycles.
+
+# F: Kosuke's Sloth
+The Fibonacci sequence, modulo k, creates a repeating sequence: 0, 1, 1.....0, 1, 1.....
+
+We need to find the nth 0, so if we can find the distance between 0s (which is constant) then we can simply multiply this by n (modulo 1E9 + 7).
+
+To do this, we can simply brute force the Fibonacci sequence until we find the 1st 0 (consider f(0) = 0 as the 0th 0).
+
+# G: Sakurako and Chefir
